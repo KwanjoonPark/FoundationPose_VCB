aa8085edf43c8db8ed7ad9b84be3251135a031b6
KwanjoonPark <parkkwanjoon@naver.com>
2026-01-21 09:04:19 +0900

    docker run for remote server

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,3 +1,11 @@
-docker rm -f foundationpose
-DIR=$(pwd)/../
-xhost +  && docker run --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -it --network=host --name foundationpose  --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $DIR:$DIR -v /home:/home -v /mnt:/mnt -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp:/tmp  --ipc=host -e DISPLAY=${DISPLAY} -e GIT_INDEX_FILE foundationpose:latest bash -c "cd $DIR && bash"
+# FOR LOCAL MACHINE
+
+# docker rm -f foundationpose
+# DIR=$(pwd)/../
+# xhost +  && docker run --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -it --network=host --name foundationpose  --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $DIR:$DIR -v /home:/home -v /mnt:/mnt -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp:/tmp  --ipc=host -e DISPLAY=${DISPLAY} -e GIT_INDEX_FILE foundationpose:latest bash -c "cd $DIR && bash"
+
+# FOR REMOTE SERVER
+
+docker rm -f foundationpose 2>/dev/null
+DIR=$(pwd)/..
+xhost + 2>/dev/null; docker run --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -it --network=host --name foundationpose --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $DIR:$DIR -v /home:/home -v /mnt:/mnt -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp:/tmp --ipc=host -e DISPLAY=${DISPLAY} -e GIT_INDEX_FILE wenbowen123/foundationpose bash -c "cd $DIR && bash"
